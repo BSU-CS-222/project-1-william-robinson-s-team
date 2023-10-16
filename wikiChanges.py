826f5e5d620ab70daae14c098569e701c9a6e3be
[William Robinson] <[william.robinson@bsu.edu]>
2023-10-16 19:47:35 -0400

    removed unnesecary line

--- wikiChanges.py
+++ wikiChanges.py
@@ -3,7 +3,6 @@ import ssl
 from urllib.request import urlopen
 
 def inputConversion(articleTitle):  #converts input article title into a wikipedia api url
-    x = articleTitle.split()
     articleConversion = articleTitle.translate({ord(i): '%20' for i in ' '})
     url = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=" + articleConversion + "&rvprop=timestamp|user&rvlimit=30&redirects"
     return url
